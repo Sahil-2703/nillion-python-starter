c12d5311ef02dccf6b3859eb9a9393a036a3b059
Sahil-2703 <rsinghsahil91@gmail.com>
2024-06-27 10:24:20 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -8,5 +8,29 @@ def nada_main():
 
     # write the computation for your program here - use my_int1 and my_int2 as inputs
     # make sure you change the output below to be your new output
+    my_int3 = SecretInteger(Input(name="my_int3", party=party2))
+    my_int4 = SecretInteger(Input(name="my_int4", party=party2))
 
-    return [Output(my_int1, "my_output", party1)]
+    # Perform some arithmetic operations
+    sum1 = my_int1 + my_int2
+    sum2 = my_int3 + my_int4
+    total_sum = sum1 + sum2
+
+    product1 = my_int1 * my_int2
+    product2 = my_int3 * my_int4
+    total_product = product1 * product2
+
+    # Conditional operations
+    is_equal = my_int1 == my_int3
+    is_greater = my_int2 > my_int4
+
+    return [  
+      Output(sum1, "sum1_output", party1),
+      Output(sum2, "sum2_output", party2),
+      Output(total_sum, "total_sum_output", party1),
+      Output(product1, "product1_output", party1),
+      Output(product2, "product2_output", party2),
+      Output(total_product, "total_product_output", party1),
+      Output(is_equal, "is_equal_output", party1),
+      Output(is_greater, "is_greater_output", party2)
+    ]
